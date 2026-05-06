ed7de152d09707cdabc604db4ced1c3e78411c9f
João Vitor Lima <joaovitorlima.5597@gmail.com>
2026-05-06 14:13:52 -0300

    config: update redirectUri for GitHub Pages production deploy

--- js/auth-config.js
+++ js/auth-config.js
@@ -4,5 +4,5 @@ const AUTH_CONFIG = {
   clientId:    '88bc4df4-c460-4730-930f-2aff0303d21d',
   tenantId:    '4bff5a2b-b30d-4939-81ff-8f76138347df',
   scopes:      ['User.Read'],
-  redirectUri: 'http://localhost:3000',
+  redirectUri: 'https://thelpoint55.github.io/MitelFlow',
 };
